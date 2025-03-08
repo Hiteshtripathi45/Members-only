d50ab09c6da2a07aaddc874597bf58d13fee448d
hitesh <hiteshtripathi91@gmail.com>
2025-03-08 15:48:24 +0530

    basic server with middlawars and view engine

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const user= require('./routes/user.router')
 
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
-app.use('/',user)
 
+
+app.use('/',user)
 app.get('/',(req,res)=>res.send('hello'))
 
 app.listen(3000,()=>console.log('server is runing'))
