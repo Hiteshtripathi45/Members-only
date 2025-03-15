c31c3e8428d4917df588c09b17e06a36b377e7ab
hitesh <hiteshtripathi91@gmail.com>
2025-03-15 19:19:25 +0530

    Fix SQL file and initialize Render database

--- db/pool.js
+++ db/pool.js
@@ -2,15 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false, // Allow self-signed certificates
-      },
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // Required for Render PostgreSQL
 });
 
+module.exports = pool;
 module.exports = pool;
